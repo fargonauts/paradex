8e41b83f3ed0c0d6e5f80fe2f411910b8c0b7e8b
Lucas Saldyt <lucassaldyt@gmail.com>
2018-04-06 23:07:36 -0700

    Adds core slipnet definitions

--- src/paradex/kernel/drivers.clj
+++ src/paradex/kernel/drivers.clj
@@ -14,7 +14,18 @@
 (def-codelet codelet-library "y" (do (println "ran y") (add-codelet coderack :z 1)))
 (def-codelet codelet-library "z" (do (println "ran z") (add-codelet coderack :x 1)))
 
+;(defn create-node [slipnet id activation depth associated]
+;  (add-node slipnet id (build-node activation depth id associated)))
+;
+;(defn create-link [slipnet from to t label length fixed]
+;  (add-link slipnet from (build-link to t label length fixed)))
+
+(def slipnet (atom {:nodes {} :links {}}))
+
 (defn main-loop []
+  (create-node slipnet "a" 100 10 [])
+  (create-node slipnet "b" 100 10 [])
+  (create-link slipnet "a" "b" nil nil 100 true)
   (if false
     nil
     (do
@@ -23,4 +34,5 @@
           (println picked)
           (run-codelet codelet-library picked)
           (println @coderack)
+          (println @slipnet)
           (recur))))))
--- src/paradex/kernel/slipnet.clj
+++ src/paradex/kernel/slipnet.clj
@@ -1,1 +1,60 @@
 (ns paradex.kernel.slipnet)
+
+; Node
+;  - activation
+;  - (conceptual) depth
+;  - name
+;  - links
+;  - codelets associated
+
+; Link
+;  - from
+;  - to
+;  - type
+;  - label
+;  - fixed length
+
+; Slipnet
+;  - Nodes
+;  - Links
+
+; That easy
+;
+; (def slipnet (atom {
+;     :nodes {:a {:activation 100 :depth 50 :associated ["x" "y"]}
+;             :b {:activation 100 :depth 50 :associated ["x" "y"]}}
+;     :links {:a {:to :b :type nil :label nil :length 50 :fixed false}}
+;   }))
+
+(defn build-node [activation depth id associated]
+  {:activation activation
+   :depth      depth
+   :id         id
+   :associated associated 
+   })
+
+(defn build-link [to t label length fixed]
+  {:to     to
+   :type   t
+   :label  label
+   :length length
+   :fixed  fixed
+   })
+
+(defn- add-nested [slipnet k1 k2 v]
+  (swap! slipnet
+         (fn [slipnet]
+           (assoc slipnet k1 
+                  (assoc (k1 slipnet) k2 v)))))
+
+(defn- add-node [slipnet k v]
+  (add-nested slipnet :nodes k v))
+
+(defn- add-link [slipnet k v]
+  (add-nested slipnet :links k v))
+
+(defn create-node [slipnet id activation depth associated]
+  (add-node slipnet id (build-node activation depth id associated)))
+
+(defn create-link [slipnet from to t label length fixed]
+  (add-link slipnet from (build-link to t label length fixed)))
